b6dccc1356ba1cb41664bb03ecb2465ff0b03d9a
Filipe <filipe.feritas.br3@gmail.com>
2021-04-21 03:39:16 -0300

    adm corno
    skubiridi papaaaaaaa

--- janela.js
+++ janela.js
@@ -1,12 +1,12 @@
 function travar() {
-    var vjhlvh = window.open('trava.html','travador brabo','height=635,width=465')
+    var vjhlvh = window.open('trava/trava.html','travador brabo','height=635,width=465')
 }
 function hook(){
-    var vjhlvh = window.open('webhook.html','','height=350,width=450')
+    var vjhlvh = window.open('webhook/webhook.html','','height=350,width=450')
 }
 
 function wide() {
-        window.open('wide.html','','height=256,width=128' )
+        window.open('putin/wide.html','','height=256,width=128' )
     
     
 }
